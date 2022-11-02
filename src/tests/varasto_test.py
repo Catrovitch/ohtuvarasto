f3297a44a7671ba829f136d192b8dff1c39ec062
Alexander <ale.engelhardt@outlook.com>
2022-11-02 12:16:32 +0200

    coveragerc, and tests for varasto

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,41 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_index_tilavuus_is_under_0(self):
+
+        varasto = Varasto(-1)
+
+        self.assertEqual(varasto.tilavuus, 0)
+
+    def test_index_saldo_is_under_0(self):
+
+        varasto = Varasto(0,-1)
+
+        self.assertEqual(varasto.saldo, 0)
+
+    def test_maara_is_under_0(self):
+
+        self.assertEqual(self.varasto.lisaa_varastoon(-1), None)
+
+    def test_maara_is_more_than_tilavuus(self):
+
+        self.varasto.lisaa_varastoon(15)
+
+        self.assertEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_ota_varastosta_under_0(self):
+
+        self.assertEqual(self.varasto.ota_varastosta(-1), 0.0)
+
+    def test_ota_varastosta_maara_is_more_than_saldo(self):
+
+        kaikki_mita_voidaan = self.varasto.ota_varastosta(5)
+
+        self.assertEqual((kaikki_mita_voidaan, self.varasto.saldo), (0.0, 0.0))
+
+    def test_str(self):
+
+        self.assertEqual(self.varasto.__str__(), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
+
+
